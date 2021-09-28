b822e793a55e42067b99439fa542adb79f64f76f
andre-tlima <andre-tlima@hotmail.com>
2021-09-28 17:08:54 +0000

    Configurando autentication e restriçoes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    
+
     validates_uniqueness_of :auth_token
-    
+    before_create :generate_authentication_token!
+
     def info
-        "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
-        
-        def generate_authentication_token!
-            begin
-                self.auth_token = Desive.friendly_token
-            end while User.exists?(auth_token: auth_token)
-                
+        "#{email} -#{created_at} - Token: #{Devise.friendly_token}"
+    end
+
+    def generate_authentication_token!
+      begin
+        self.auth_token = Devise.friendly_token
+      end while User.exists?(auth_token: auth_token)        
     end
 end
